b51b0fd02807b3c40086b58ee434860ea37fa2e9
Basile Paoli <basile.paoli@gmail.com>
2025-12-27 16:53:48 +0100

    add review date calculation

--- src/domain/services/LeitnerSchedulingService.ts
+++ src/domain/services/LeitnerSchedulingService.ts
@@ -1,6 +1,7 @@
 import { Service } from "typedi";
 import { Card } from "../models/Card";
 import { Category } from "../models/Category";
+import { Review } from "../models/Review";
 
 const CORRECT_REVIEW_COUNT_CATEGORY_MAP: Record<number, Category> = {
   0: Category.First,
@@ -13,12 +14,21 @@ const CORRECT_REVIEW_COUNT_CATEGORY_MAP: Record<number, Category> = {
   7: Category.Done,
 };
 
+const CATEGORY_DAYS_INTERVAL_MAP: Record<Category, number> = {
+  [Category.First]: 1,
+  [Category.Second]: 2,
+  [Category.Third]: 4,
+  [Category.Fourth]: 8,
+  [Category.Fifth]: 16,
+  [Category.Sixth]: 32,
+  [Category.Seventh]: 64,
+  [Category.Done]: 0,
+};
+
 @Service()
 export class LeitnerSchedulingService {
   getCardCategory(card: Card): Category {
-    const sortedReviews = [...card.reviews].sort(
-      (a, b) => a.reviewedAt.getTime() - b.reviewedAt.getTime()
-    );
+    const sortedReviews = this.getSortedReviews(card);
     const consecutiveCorrectReviews = sortedReviews.reduce((count, review) => {
       if (review.isCorrect) {
         return count + 1;
@@ -37,4 +47,29 @@ export class LeitnerSchedulingService {
     return category;
   }
 
+  shouldReviewCard(card: Card, onDate: Date): boolean {
+    const nextReviewDate = this.getNextReviewDate(card);
+    if (!nextReviewDate) {
+      return false;
+    }
+    return nextReviewDate <= onDate;
+  }
+
+  private getNextReviewDate(card: Card): Date | null {
+    const category = this.getCardCategory(card);
+    if (category === Category.Done) {
+      return null;
+    }
+    const intervalDays = CATEGORY_DAYS_INTERVAL_MAP[category];
+    const lastReviewDate =
+      this.getSortedReviews(card).pop()?.reviewedAt || card.createdAt;
+    const intervalMs = intervalDays * 24 * 60 * 60 * 1000;
+    return new Date(lastReviewDate.getTime() + intervalMs);
+  }
+
+  private getSortedReviews(card: Card): Review[] {
+    return [...card.reviews].sort(
+      (a, b) => a.reviewedAt.getTime() - b.reviewedAt.getTime()
+    );
+  }
 }
